2a9f446aaefb8a5eb70bc6dd22ec60cf017a6ec7
Brandon <brandon5.9@hotmail.com>
2025-11-22 19:51:09 -0500

    Name, address, date set up

--- client/src/Content.js
+++ client/src/Content.js
@@ -1,8 +1,11 @@
 import pawsPrint from './photos/pawsPrint.png'
 import dogWalker from './photos/dogWalker.png'
+import { useState } from 'react';
 
 
 const Content = () => {
+
+    const [formData, setFormData] = useState({ name: '' });
 
 
         return (
@@ -22,10 +25,37 @@ const Content = () => {
                 
 
                 <div className="right-container">
-                    <div className='form'></div>
+                    <div className='form'>
+                        
+                        <div className="form-inputs">
+                            <input
+                            type="text"
+                            value={formData.name}
+                            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+                            placeholder="Name of My Dog"
+                            className="form-name"
+                            />
+
+                             <input
+                            type="text"
+                            value={formData.address}
+                            onChange={(e) => setFormData({ ...formData, address: e.target.value })}
+                            placeholder="Address"
+                            />
+
+
+                            <input
+                                type="datetime-local"
+                                name="dateTime"
+                                value={formData.dateTime}
+                                onChange={(e) => setFormData({ ...formData, [e.target.name]: e.target.value })}
+                                required
+                                className="form-datetime"
+                            />
+
+                        </div>
+                    </div>
                 </div>
-
-
 
                 <hr className="section-divider" />
 
